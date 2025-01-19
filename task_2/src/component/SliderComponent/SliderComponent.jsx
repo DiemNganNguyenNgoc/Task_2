e67aeb4a0fe982a341e224caa4f1a77ac6a6b7ef
DiemNganNguyenNgoc <22520933@gm.uit.edu.vn>
2025-01-20 01:19:51 +0700

    demo1

--- task_2/src/component/SliderComponent/SliderComponent.jsx
+++ task_2/src/component/SliderComponent/SliderComponent.jsx
@@ -8,27 +8,31 @@ const SliderComponent = ({ arrImg }) => {
     infinite: true,
     slidesToShow: 6,
     slidesToScroll: 1,
-    arrows: true,
+    arrows: true
   };
 
   return (
     <div className="slider-container">
       <Slider {...settings}>
-        {arrImg.map((image, index) => (
-          <div
-            key={index}
-            className="slider-item"
-            style={{
-              marginTop: index % 2 === 0 ? "50px" : "0", // Kiểm tra index chẵn hay lẻ
-            }}
-          >
-            <img
-              className="slider-image"
-              src={image}
-              alt={`slider-${index}`}
-            />
-          </div>
-        ))}
+        {arrImg.map((image, index) => {
+          console.log(`Index: ${index}`);
+          
+          return (
+            <div
+              key={index}
+              className="slider-item"
+            >
+              <img
+                className="slider-image"
+                src={image}
+                alt={`slider-${index}`}
+                style={{
+                  marginTop: index % 2 === 0 ? "30px" : "0", // Kiểm tra index chẵn hay lẻ
+                }}
+              />
+            </div>
+          );
+        })}
       </Slider>
     </div>
   );
